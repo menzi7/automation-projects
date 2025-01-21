56398deb1dbb1e6c9f1d2cc24f64d1594062ade0
Tobias Menzi <79048220+menzi7@users.noreply.github.com>
2025-01-21 08:57:18 +0100

    Added Start-Countdown

--- PSfunctions.psm1
+++ PSfunctions.psm1
@@ -1,71 +1,73 @@
 function ConvertTo-HtmlTable {
     <#
-.SYNOPSIS
+    .SYNOPSIS
     Convert array to HTML table.
-.DESCRIPTION
+    .DESCRIPTION
     The function will convert an array to String, and return array as a HTML table.
-.PARAMETER InputObject
+    .PARAMETER InputObject
     When specified, the funtion converts InputObject to a HTML table.
-.EXAMPLE
+    .EXAMPLE
     PS> ConvertTo-HtmlTable -InputObject $report
-.OUTPUTS
+    .OUTPUTS
     System.String. ConvertTo-HtmlTable returns HTML as String.
-#>
+    #>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)] 
         [PSObject[]]$InputObject
     )
-    
+
     begin {
-        $header = $InputObject[0].psobject.properties.name 
-        $table = "<table><thead><tr>" 
-        foreach ($h in $header) { 
-            $table += "<th>$h</th>" 
-        } 
+        $header = $InputObject[0].psobject.properties.name
+        $table = "<table><thead><tr>"
+        foreach ($h in $header) {
+            $table += "<th>$h</th>"
+        }
         $table += "</tr></thead><tbody>"
     }
-    
+
     process {
-        foreach ($line in $InputObject) { 
-            $table += "<tr>" 
-            foreach ($h in $header) { 
-                $table += "<td>$($line.$h)</td>" 
+        foreach ($line in $InputObject) {
+            $table += "<tr>"
+            foreach ($h in $header) {
+                $table += "<td>$($line.$h)</td>"
             } 
-            $table += "</tr>" 
+            $table += "</tr>"
         }
     }
-    
+
     end {
-        $table += "</tbody></table>" 
+        $table += "</tbody></table>"
         $table
     }
 }
 
 function Remove-DNSRecords {
     <#
-.SYNOPSIS
+    .SYNOPSIS
     Remove DNS records across all zones
-.DESCRIPTION
+    .DESCRIPTION
     This function will search for a hostname across all zones and attempt to remove them.
     Best practice is running this script directly from the DC/DNS server itself.
-.NOTES 
+    .NOTES
     Author: TME
-.PARAMETER DNSserver
+    .PARAMETER DNSserver
     Define DNS server. Remove-DNSRecords defaults to localhost. FQDN
-.PARAMETER Server
+    .PARAMETER Server
     Define the name of the server that needs to be removed from DNS. Not FQDN
-.EXAMPLE
+    .EXAMPLE
     PS> Remove-DNSRecords -Server $ServerToDelete
-.EXAMPLE
+    .EXAMPLE
     PS> Remove-DNSRecords -Server $ServerToDelete -DNSserver DC01.Contoso.local
-#>
+    #>
+
     [CmdletBinding()]
     param (
         [string]$DNSserver = "localhost",
         [Parameter(Mandatory = $true)][string]$Server
     )
-    
+
     $fwdRecord = $null
     $revRecord = $null
 
@@ -104,17 +106,17 @@ function Remove-DNSRecords {
 }
 
 function Get-HostInfo {
-
     <#
-.SYNOPSIS
+    .SYNOPSIS
     Gets ESXi Version, Hardware and CPU Type from hosts.
-.DESCRIPTION
+    .DESCRIPTION
     This script will get ESXi Version, Hardware and CPU Type from all hosts, that is available from currently connected vCentres.
-.NOTES
+    .NOTES
     Author: TME
-.EXAMPLE
+    Connection to vCenter needs to be established beforehand.
+    .EXAMPLE
     PS> Get-HostInfo
-#>
+    #>
 
     get-vmhost | Select-Object Name,
     @{N = 'ESXi version'; E = { "$($_.Version) $($_.Build)" } },
@@ -124,26 +126,26 @@ function Get-HostInfo {
 }
 
 function Search-ScheduledTasks {
-
     <#
-.SYNOPSIS
+    .SYNOPSIS
     Search for scheduled tasks in vCenter
-.NOTES
+    .NOTES
     Author: TME
-.EXAMPLE
+    Connection to vCenter needs to be established beforehand.
+    .EXAMPLE
     PS> Search-ScheduledTasks
     Show all scheduled tasks.
-
+    .EXAMPLE
     PS> Search-ScheduledTasks -Keyword "UIT"
     Show all scheduled tasks with "UIT" in the name.
-#>
+    #>
 
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)][string]$Keyword
     )
 
-    # Get all 
+    # Get all
     $ScheduledTask = (Get-View ScheduledTaskManager).ScheduledTask | % { (Get-View $_).Info }
 
     # Show all names and next run time
@@ -159,4 +161,39 @@ function Search-ScheduledTasks {
         $ScheduledTask | Select-Object Name, NextRunTime | Sort-Object Name
     }
 
+}
+
+Function Start-Countdown {
+    <#
+    .SYNOPSIS
+    Provide a graphical countdown if you need to pause a script for a period of time
+    .PARAMETER Seconds
+    Time, in seconds, that the function will pause
+    .PARAMETER Messge
+    Message you want displayed while waiting
+    .EXAMPLE
+    Start-Countdown -Seconds 30 -Message Please wait while Active Directory replicates data...
+    .NOTES
+        Author:            Martin Pugh
+        Twitter:           @thesurlyadm1n
+        Spiceworks:        Martin9700
+        Blog:              www.thesurlyadmin.com
+
+        Changelog:
+           2.0             New release uses Write-Progress for graphical display while couting
+                           down.
+           1.0             Initial Release
+    .LINK
+    http://community.spiceworks.com/scripts/show/1712-start-countdown
+    #>
+
+    Param(
+        [Int32]$Seconds = 10,
+        [string]$Message = "Pausing for 10 seconds..."
+    )
+    ForEach ($Count in (1..$Seconds)) {
+        Write-Progress -Id 1 -Activity $Message -Status "Waiting for $Seconds seconds, $($Seconds - $Count) left" -PercentComplete (($Count / $Seconds) * 100)
+        Start-Sleep -Seconds 1
+    }
+    Write-Progress -Id 1 -Activity $Message -Status "Completed" -PercentComplete 100 -Completed
 }
